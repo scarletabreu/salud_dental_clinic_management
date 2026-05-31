aaf8fba539639289959b7e2e66a5c19c27062b51
Underscore-IDPD <IDPD0001@ce.pucmm.edu.do>
2026-05-31 18:56:30 -0400

    feat(tratamientos): Tratamientos CRUD

--- lib/features/tratamiento/data/datasources/tratamiento_remote_datasource_impl.dart
+++ lib/features/tratamiento/data/datasources/tratamiento_remote_datasource_impl.dart
@@ -11,7 +11,7 @@ class TratamientoRemoteDatasourceImpl implements TratamientoRemoteDatasource {
     try {
       final response = await supabaseClient
           .from('tratamientos')
-          .select('*, contraindicaciones:tratamiento_id(*)')
+          .select('*, contraindicaciones(*)')
           .filter('deleted_at', 'is', null)
           .order('nombre', ascending: true);
 
